f4a47c1645295a43448aff16764b70320ddc176e
Navojith <navojith22@gmail.com>
2024-05-10 14:33:09 +0530

    feat: my courses ui

--- src/pages/myCourses/MyCourses.tsx
+++ src/pages/myCourses/MyCourses.tsx
@@ -1,7 +1,72 @@
+import { useEffect, useState } from 'react';
 import PageContainer from '../../components/PageContainer/PageContainer';
+import { getMyCoursesWithProgression } from '../../services/progressionService';
+import { useNavigate } from 'react-router-dom';
+import { COURSE } from '../../routes/route.json';
+
+interface Progression {
+  completedSteps: number;
+  userId: string;
+  id: string;
+  courseDetails: {
+    courseId: string;
+    name: string;
+    description: string;
+  };
+}
 
 function MyCourses() {
-  return <PageContainer>MyCourses</PageContainer>;
+  const [courses, setCourses] = useState<Progression[]>([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchCourses = async () => {
+      try {
+        const response = await getMyCoursesWithProgression(
+          '662e30a75b0e59f7fb038023'
+        );
+        if (response) {
+          setCourses(response);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchCourses();
+  }, []);
+  return (
+    <PageContainer>
+      <h2 className="text-3xl font-bold">My Courses</h2>
+      <div className="mt-10 flex flex-col gap-5">
+        {courses?.map((course) => {
+          return (
+            <div
+              key={course.id}
+              className={
+                'border border-secondary bg-primaryLighter hover:bg-primaryDark cursor-pointer p-3 md:px-10 md:py-5 rounded flex flex-row gap-2 items-center'
+              }
+              onClick={() =>
+                navigate(`${COURSE.route}/${course.courseDetails.courseId}`)
+              }
+            >
+              <div className="flex  flex-col gap-2">
+                <h3 className="text-xl font-bold">
+                  {course.courseDetails.name}
+                </h3>
+                <p>{course.courseDetails.description}</p>
+                <p>Progress: {course.completedSteps}%</p>
+              </div>
+              <div className="ml-auto mr-5">
+                <button className="bg-primaryLighter hover:bg-primaryDarker border-text">
+                  Go to Course
+                </button>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </PageContainer>
+  );
 }
 
 export default MyCourses;
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,26 @@ export default {
   theme: {
     extend: {
       colors: {
-        primaryLighter: '#191e24',
-        primaryDarker: '#0a0a0a',
-        secondary: '#7c3aed',
-        text: '#f5f3ff',
+        primaryLighter: '#191e24', // navbar color + secondary bg
+        primaryDarker: '#0a0a0a', // main background color
+        primaryDark: '#111418', // secondary bg hover color
+        secondary: '#7c3aed', // purple
+        text: '#f5f3ff', // text
       },
     },
   },
   plugins: [require('daisyui')],
+  daisyui: {
+    themes: [
+      {
+        myTheme: {
+          primaryLighter: '#191e24', // navbar color + secondary bg
+          primaryDarker: '#0a0a0a', // main background color
+          primaryDark: '#111418', // secondary bg hover color
+          secondary: '#7c3aed', // purple
+          text: '#f5f3ff', // text
+        },
+      },
+    ],
+  },
 };

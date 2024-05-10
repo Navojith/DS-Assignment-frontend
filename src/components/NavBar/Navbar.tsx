a6dbf27138992b270da390ae1237a2164e6f03d5
Navojith <navojith22@gmail.com>
2024-05-10 20:37:02 +0530

    feat: progress

--- src/components/NavBar/Navbar.tsx
+++ src/components/NavBar/Navbar.tsx
@@ -26,7 +26,7 @@ const Navbar = () => {
           </div>
           <ul
             tabIndex={0}
-            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-primaryLighter rounded-box w-52"
           >
             <li>
               <Link
@@ -74,7 +74,7 @@ const Navbar = () => {
         </Link>
       </div>
       <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
+        <ul className="menu menu-horizontal px-1 bg-primaryLighter">
           <li>
             <Link to={'./'}>Home</Link>
           </li>
